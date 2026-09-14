4bc8e417e4700c3590611110a1b2520c04e2a81e
Diego José de Melo <diegomeloads@gmail.com>
2026-09-14 16:47:27 -0300

    update application version to 1.1.0 and enhance deployment configuration with health checks and environment variables

--- app/app.py
+++ app/app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 def home():
     return jsonify({
         "application": "kubernetes-gitops-api",
-        "version": "1.0.0",
+        "version": "1.1.0",
         "environment": os.getenv("APP_ENV", "unknown")
     })
 
